316af0c089c0de80ffeca74c5ae896661ef1d069
José Serrano <ze_carapuca@hotmail.com>
2016-11-29 19:20:15 +0000

    Revert "Modified struct and defined dependencies"
    This reverts commit fd013aaf19c1b1900b1e5c62ee4c00b972dddb82.

--- CGJ_Team/CGJ_Team/engine.h
+++ CGJ_Team/CGJ_Team/engine.h
@@ -5,8 +5,8 @@
 #define RADIANS_TO_DEGREES 57.29577951308232185913
 #define M_UNITE 0.7071067811865475244f
 
-#include "GL\glew.h"
-#include "GL\freeglut.h"
+#include "Dependencies\glew\glew.h"
+#include "Dependencies\freeglut\freeglut.h"
 
 #include <iostream>
 #include <sstream>
